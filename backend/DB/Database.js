367ed97dedf28e33eba220e958aff0ee53a38be9
Manish Patil <manishpatil3253@gmail.com>
2025-09-13 10:42:35 +0530

    Initial commit with CORS fix

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -1,10 +1,24 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const connectDB = async (req, res) => {
-    const db = process.env.MONGO_URL;
+// export const connectDB = async (req, res) => {
+//     const db = process.env.MONGO_URL;
 
-    const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
+//     const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
 
-    console.log(`MongoDB Connected to ${connection.host}`);
+//     console.log(`MongoDB Connected to ${connection.host}`);
 
-}
+// }
+
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        // Make sure you are using process.env.MONGO_URI here
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connection SUCCESS');
+    } catch (error) {
+        console.log('MongoDB connection FAILED');
+        console.error(error);
+        process.exit(1);
+    }
+};
